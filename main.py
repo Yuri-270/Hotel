5398910bb90f985236750911e0b1a7953d7f515b
Yuri Klimuk <270720contra@gmail.com>
2024-03-16 17:46:08 +0200

    created connect to DB and starting bot

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from asyncio import run
+import logging
+import sys
 
+from aiogram import Bot, Dispatcher
+import asyncpg
+
+from config import *
+from utils.data_base import DataBase
+
+
+async def main():
+    bot = Bot(TOKEN_API)
+    dp = Dispatcher()
+
+    pool = await asyncpg.create_pool(
+        host=db_data['host'],
+        port=db_data['port'],
+        user=db_data['user'],
+        password=db_data['password'],
+        database=db_data['database_name']
+    )
+    DataBase(pool)
+    await DataBase.create_db()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        run(main())
+    except KeyboardInterrupt:
+        print('Bot is closed')
