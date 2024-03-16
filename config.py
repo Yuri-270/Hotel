b10c751e9aeaa33ef2059684980ff72547810d4c
Yuri Klimuk <270720contra@gmail.com>
2024-03-16 16:40:07 +0200

    Create config file

---
+++ config.py
@@ -0,0 +1,10 @@
+
+TOKEN_API = '7111799958:AAFwJxf6jxihwSn2eJF_4KIAzabhIpT1d5w'
+
+db_data = {
+    'host': 'localhost',
+    'port': 5432,
+    'user': 'postgres',
+    'password': '4r3e2w!Q',
+    'database_name': 'Hotel_Kovel_DB'
+}
