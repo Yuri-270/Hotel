776a37cf726d00e48dbd31e1288d4e0f1b211aa5
Yuri Klimuk <270720contra@gmail.com>
2024-05-28 16:53:02 +0300

    Add method, what check, if user introduced all personal data

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -1,10 +1,10 @@
-from aiogram import Bot
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 
 from handlers.support import SupportClass
 from handlers.rent_a_room import RentARoom
 from utils.states import SelectHotel
+from utils.data_base import DataBase
 
 
 __all__ = [
@@ -19,11 +19,11 @@ class MainHandler(SupportClass):
         super().__init__()
         self._rent_a_room_kb()
 
-    async def main_handler(self, message: Message, state: FSMContext, bot: Bot):
+    async def main_handler(self, message: Message, state: FSMContext):
         match message.text:
 
             case 'Орендувати кімнату 🏙':
-                await self.rent_a_room(message, state, bot)
+                await self.rent_a_room(message, state)
 
             case 'Настройки ⚙️':
                 await self.settings(message, state)
@@ -37,7 +37,8 @@ class MainHandler(SupportClass):
             case _:
                 await message.answer("Виберіть з меню ⬇️")
 
-    async def rent_a_room(self, message: Message, state: FSMContext, bot: Bot):
+    async def rent_a_room(self, message: Message, state: FSMContext):
+        await self.__check_authorization_data(message, state)
         state_data = await state.get_data()
         if state_data['CITY'] is None:
             await message.answer(
@@ -49,7 +50,33 @@ class MainHandler(SupportClass):
         else:
             await self.rent_a_room_class.get_hotels(message, state)
 
-    async def set_location(self, message: Message, state: FSMContext, bot: Bot):
+    @staticmethod
+    async def __check_authorization_data(message: Message, state: FSMContext) -> bool:
+        state_data = await state.get_data()
+        pool = await DataBase.get_pool()
+        async with pool.acquire() as con:
+            users_data = await con.fetch(
+                "SELECT * FROM users WHERE id = $1",
+                state_data['USER_ID']
+            )
+
+            all_data_confirm = True
+            if users_data['email'] is not None:
+                all_data_confirm = False
+            elif users_data['telephone_number']:
+                all_data_confirm = False
+            elif users_data['passport_number']:
+                all_data_confirm = False
+            elif users_data['passport_valid_until']:
+                all_data_confirm = False
+
+            if all_data_confirm is False:
+                await message.answer("""Ви не вказали всі дані щоб бронювати кімнати
+Пока що ви можете тише їх лише переглядати""")
+
+        return all_data_confirm
+
+    async def set_location(self, message: Message, state: FSMContext):
         if message.location:
             user_city = await self.get_city_name(
                 message.location.latitude,
@@ -57,7 +84,7 @@ class MainHandler(SupportClass):
             )
             await message.answer(f"Вибрано місто <b>{user_city.capitalize()}</b>", parse_mode='HTML')
             await state.update_data(CITY=user_city.capitalize())
-            await self.rent_a_room(message, state, bot)
+            await self.rent_a_room(message, state)
 
         elif message.text == "На головне меню ⤵️":
             await self.main_menu(message, state)
@@ -65,7 +92,7 @@ class MainHandler(SupportClass):
         else:
             await message.answer(f"Вибрано місто <b>{message.text.capitalize()}</b>", parse_mode='HTML')
             await state.update_data(CITY=message.text.capitalize())
-            await self.rent_a_room(message, state, bot)
+            await self.rent_a_room(message, state)
 
     async def settings(self, message: Message, state: FSMContext):
         pass
